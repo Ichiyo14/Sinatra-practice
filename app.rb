2025bc3da9ac9d38f6aa36faef4f750ebba0141a
yoichi <itiyou14@gmail.com>
2021-05-13 17:14:52 +0900

    File save method format to json

--- app.rb
+++ app.rb
@@ -1,73 +1,75 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'securerandom'
+require 'json'
 
 post '/memos' do
-    id = SecureRandom.uuid
-    @title = "#{params['title']}"
-    @content = "#{params['content']}"
-    make_data(id, @title, @content)
-    redirect '/memos'
+  id = SecureRandom.uuid
+  @title = params['title'].to_s
+  @content = params['content'].to_s
+  make_data(id, @title, @content)
+  redirect '/memos'
 end
 
 get '/memos' do
-    @memos = []
-    Dir.glob("*", base:"memos").sort_by{|f| File.mtime("memos/#{f}")}.each do |id|
-        @memos << use_data(id)
-    end
-    @memos
-    erb :index
+  @memos = []
+  Dir.glob('*', base: 'memos').sort_by { |f| File.mtime("memos/#{f}") }.each do |id|
+    @memos << use_data(id)
+  end
+  erb :index
 end
 
 get '/memos/new' do
-    erb :new
+  erb :new
 end
 
 get '/memos/:id/edit' do |id|
-    hash = use_data(id)
-    @id = "#{id}"
-    @title = hash[:title]
-    @content = hash[:content]
-    erb :edit
+  hash = use_data(id)
+  @id = id.to_s
+  @title = hash[:title]
+  @content = hash[:content]
+  erb :edit
 end
 
 get '/memos/:id' do |id|
-    hash = use_data(id)
-    @id = "#{id}"
-    @title = hash[:title]
-    @content = hash[:content]
-    erb :memo
+  hash = use_data(id)
+  @id = id.to_s
+  @title = hash[:title]
+  @content = hash[:content]
+  erb :memo
 end
 
-delete  '/memos/:id' do |id|
-    File.delete("memos/#{id}")
-    redirect '/memos'
+delete '/memos/:id' do |id|
+  File.delete("memos/#{id}")
+  redirect '/memos'
 end
 
 patch '/memos/:id' do |id|
-    @title = "#{params['title']}"
-    @content = "#{params['content']}"
-    make_data(id, @title, @content)
-    redirect '/memos'
+  @title = params['title'].to_s
+  @content = params['content'].to_s
+  make_data(id, @title, @content)
+  redirect '/memos'
 end
 
 def make_data(id, title, content)
-    memo_date = {id: id, title: title, content: content}
-    to_contener(memo_date, id)
+  memo_data = { id: id, title: title, content: content }
+  json_memo_data = JSON.dump(memo_data)
+  to_file(json_memo_data, id)
 end
 
-def to_contener(memo_date, id)
-    File.open("memos/#{id}","w") do |f|
-        f.puts(memo_date)
-    end
+def to_file(memo_data, id)
+  File.open("memos/#{id}", 'w') do |f|
+    f.puts(memo_data)
+  end
 end
 
 def use_data(id)
-    (eval File.read("memos/#{id}"))
+  p a = File.read("memos/#{id}")
+  JSON.parse(a, symbolize_names: true)
 end
 
 # def puts_memos_list
 #     Dir.glob("*", base:"memos")
 #     end
-
-
