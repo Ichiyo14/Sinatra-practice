b2545cd2ef2cd0b29b7a378926b24eafa1fb7049
yoichi <itiyou14@gmail.com>
2021-05-18 18:17:31 +0900

    Fix XSS/Implement measures against Directory Traversal

--- app.rb
+++ app.rb
@@ -8,20 +8,27 @@ require 'erb'
 
 helpers do
   def h(text)
-    escape_html(text)
+    Rack::Utils.escape_html(text)
   end
+end
+
+get '/' do
+  redirect '/memos'
 end
 
 post '/memos' do
   id = SecureRandom.uuid
-  @title = h(params['title']).to_s
-  @content = h(params['content']).to_s
-  make_data(id, @title, @content)
+  @title = (params['title']).to_s
+  @content = (params['content']).to_s
+    make_data(id, @title, @content)
   redirect '/memos'
 end
 
 get '/memos' do
-  @memos = Dir.glob('*', base: 'memos').sort_by { |f| File.mtime("memos/#{f}") }.reverse.map { |id| use_data(id) }
+  @memos = Dir.glob('*', base: 'memos').sort_by { |f| File.mtime("memos/#{f}") }.reverse.map do |file|
+    id = file.delete_prefix('memo_')
+    use_data(id)
+  end
   erb :index
 end
 
@@ -31,28 +38,28 @@ end
 
 get '/memos/:id/edit' do |id|
   hash = use_data(id)
-  @id = id.to_s
-  @title = hash[:title]
-  @content = hash[:content]
+  @id = h(id.to_s)
+  @title = h(hash[:title])
+  @content = h(hash[:content])
   erb :edit
 end
 
 get '/memos/:id' do |id|
   hash = use_data(id)
-  @id = id.to_s
-  @title = hash[:title]
-  @content = hash[:content]
+  @id = h(id.to_s)
+  @title = h(hash[:title])
+  @content = h(hash[:content])
   erb :memo
 end
 
 delete '/memos/:id' do |id|
-  File.delete("memos/#{id}")
+  File.delete("memos/memo_#{id}")
   redirect '/memos'
 end
 
 patch '/memos/:id' do |id|
-  @title = h(params['title']).to_s
-  @content = h(params['content']).to_s
+  @title = (params['title']).to_s
+  @content = (params['content']).to_s
   make_data(id, @title, @content)
   redirect '/memos'
 end
@@ -64,11 +71,11 @@ def make_data(id, title, content)
 end
 
 def to_file(memo_data, id)
-  File.open("memos/#{id}", 'w') do |f|
+  File.open("memos/memo_#{id}", 'w') do |f|
     f.puts(memo_data)
   end
 end
 
 def use_data(id)
-  JSON.parse(File.read("memos/#{id}"), symbolize_names: true)
+  JSON.parse(File.read("memos/memo_#{id}"), symbolize_names: true)
 end
