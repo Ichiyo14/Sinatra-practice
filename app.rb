0f70272cbab9cd50c3d425d894614307563adbdc
yoichi <itiyou14@gmail.com>
2021-05-20 11:12:06 +0900

    Chenge memos save method to database

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ require 'sinatra/reloader'
 require 'securerandom'
 require 'json'
 require 'erb'
+require 'pg'
+
+CONN = PG::Connection.open(dbname: 'memo_app')
 
 helpers do
   def h(text)
@@ -18,17 +21,14 @@ end
 
 post '/memos' do
   id = SecureRandom.uuid
-  @title = (params['title']).to_s
-  @content = (params['content']).to_s
-    make_data(id, @title, @content)
+  @title = params['title'].to_s
+  @content = params['content'].to_s
+  make_data(id, @title, @content)
   redirect '/memos'
 end
 
 get '/memos' do
-  @memos = Dir.glob('*', base: 'memos').sort_by { |f| File.mtime("memos/#{f}") }.reverse.map do |file|
-    id = file.delete_prefix('memo_')
-    use_data(id)
-  end
+  @memos = CONN.exec('SELECT * FROM memos').map { |memo_data| memo_data }.reverse
   erb :index
 end
 
@@ -39,43 +39,43 @@ end
 get '/memos/:id/edit' do |id|
   hash = use_data(id)
   @id = h(id.to_s)
-  @title = h(hash[:title])
-  @content = h(hash[:content])
+  @title = h(hash[0]['title'])
+  @content = h(hash[0]['content'])
   erb :edit
 end
 
 get '/memos/:id' do |id|
   hash = use_data(id)
   @id = h(id.to_s)
-  @title = h(hash[:title])
-  @content = h(hash[:content])
+  @title = h(hash[0]['title'])
+  @content = h(hash[0]['content'])
   erb :memo
 end
 
 delete '/memos/:id' do |id|
-  File.delete("memos/memo_#{id}")
+  delete_data(id)
   redirect '/memos'
 end
 
 patch '/memos/:id' do |id|
-  @title = (params['title']).to_s
-  @content = (params['content']).to_s
-  make_data(id, @title, @content)
+  @title = params['title'].to_s
+  @content = params['content'].to_s
+  patch_data(id, @title, @content)
   redirect '/memos'
 end
 
 def make_data(id, title, content)
-  memo_data = { id: id, title: title, content: content }
-  json_memo_data = JSON.dump(memo_data)
-  to_file(json_memo_data, id)
-end
-
-def to_file(memo_data, id)
-  File.open("memos/memo_#{id}", 'w') do |f|
-    f.puts(memo_data)
-  end
+  CONN.exec('INSERT INTO memos VALUES($1, $2, $3);', [id, title, content])
 end
 
 def use_data(id)
-  JSON.parse(File.read("memos/memo_#{id}"), symbolize_names: true)
+  CONN.exec('SELECT * FROM memos WHERE id=$1;', [id])
+end
+
+def patch_data(id, title, content)
+  CONN.exec('UPDATE memos SET title = $2 , content = $3 WHERE id = $1;', [id, title, content])
+end
+
+def delete_data(id)
+  CONN.exec('DELETE FROM memos WHERE id = $1;', [id])
 end
